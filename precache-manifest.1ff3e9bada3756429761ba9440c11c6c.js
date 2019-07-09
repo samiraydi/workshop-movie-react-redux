de102141d8d951a99cf23fea81c86c074a02477a
samiraydi <samiraydi26@gmail.com>
2019-07-10 00:09:03 +0100

    Updates

---
+++ precache-manifest.1ff3e9bada3756429761ba9440c11c6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748bcc3795d2debdc77d9106d9264545",
+    "url": "/workshop-movie-react-redux/index.html"
+  },
+  {
+    "revision": "d4b24598547f360f11cc",
+    "url": "/workshop-movie-react-redux/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a8b3a3b8de7da156fb1f",
+    "url": "/workshop-movie-react-redux/static/js/2.37038ee1.chunk.js"
+  },
+  {
+    "revision": "d4b24598547f360f11cc",
+    "url": "/workshop-movie-react-redux/static/js/main.621b89d2.chunk.js"
+  },
+  {
+    "revision": "faa845b35fa71622a546",
+    "url": "/workshop-movie-react-redux/static/js/runtime~main.3727a0d4.js"
+  }
+]);
